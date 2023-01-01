512ed4da8dadf8dc62626f53553ec349c284c2e2
Somnath Dey <sdey.aklakhi@gmail.com>
2023-01-01 07:50:04 +0530

    Initial commit of second lecture.I just coppied the first leccode to it

---
+++ 02. Igniting our App/App.js
@@ -0,0 +1,37 @@
+// const heading = React.createElement("h1", { id: "title", className: "hey" }, "Namaste Everyone!");
+
+// console.log(heading)
+// //If we console.log the heading we can see the heading is an object.
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// // Passing a react element inside the root
+// root.render(heading);
+
+//................................................
+const heading1 = React.createElement(
+  "h1",
+  { id: "title1", className: "hey1" },
+  "Heading1"
+);
+
+const heading2 = React.createElement(
+  "h2",
+  {
+    id: "title2",
+    className: "hey2",
+    // and not only id or class name we can  pass any props inside this curly braces.
+    hello: "world",
+  },
+  "Heading2"
+);
+
+const container = React.createElement("div", { id: "container" }, [
+  heading1,
+  heading2,
+]);
+// in React when we pass multiple children, it should pass inside an array like above.
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
