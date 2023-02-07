7c48f8aad2562860802c94591dabd7277645bc14
Somnath Dey <sdey.aklakhi@gmail.com>
2023-02-07 10:11:49 +0530

    start session 09

---
+++ 09. Optimizing our App/src/App.js
@@ -0,0 +1,58 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HeaderComponent from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
+import Profile from "./components/Profile";
+
+const AppLayout = () => {
+  return (
+    <>
+      <HeaderComponent />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+//always create this at below
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+        children: [
+          {
+            path: "profile",
+            element: <Profile />,
+          },
+        ],
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:resId",
+        element: <RestaurantMenu />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
---
+++ 09. Optimizing our App/src/components/ProfileClass.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    //Create State
+    this.state = {
+      userInfo: {
+        name: "Dummy Name",
+        location: "Dummy Loation",
+      },
+    };
+    console.log("Child-constructor" + this.props.name);
+  }
+
+  async componentDidMount() {
+    //Best place to make a api call
+    const data = await fetch("https://api.github.com/users/thecodophile");
+    const json = await data.json();
+
+    console.log(json);
+
+    this.setState({
+      userInfo: json,
+    });
+    console.log("Child-componentDidMount" + this.props.name);
+  }
+
+  componentDidUpdate() {
+    console.log("Old component Update");
+  }
+
+  componentWillUnmount() {
+    console.log("ComponentWillUnmount");
+  }
+
+  render() {
+    console.log("Child-Render" + this.props.name);
+    //I just do destructuring and optional chaining
+    const { avatar_url, name, location } = this?.state?.userInfo;
+    return (
+      <div>
+        <h1>Profile Class Component</h1>
+        <img src={avatar_url} />
+        <p>Name: {name}</p>
+        <p>Location: {location}</p>
+      </div>
+    );
+  }
+}
+export default Profile;
+
+/**
+ * Render phase***************
+ * Parent Constructor
+ * parent Render
+ * Child Constructor
+ * Child Render
+ *
+ * Commit Phase***************
+ * DOM is updated API call
+ * json loged
+ * Child compomentDidMount
+ * Parent componentDidMount
+ *
+ */
