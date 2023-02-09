33f04e65b157861c5a338fcd0005c249ac8d1d3f
Somnath Dey <sdey.aklakhi@gmail.com>
2023-02-09 17:30:24 +0530

    learn props drilling and learn to create simple accordion

--- 11. Data is the new Oil/src/components/Instamart.js
+++ 11. Data is the new Oil/src/components/Instamart.js
@@ -1,10 +1,54 @@
+import { useState } from "react";
+
+const Section = ({ title, description }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <div className="border border-black p-2 m-2">
+      <h2 className="text-xl font-bold">{title}</h2>
+      {isVisible ? (
+        <button
+          onClick={() => setIsVisible(false)}
+          className="border border-green-400 bg-green-200 cursor-pointer text-sm m-2"
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          onClick={() => setIsVisible(true)}
+          className="border border-green-400 bg-green-200 cursor-pointer text-sm m-2"
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
+
 const Instamart = () => {
   return (
-    <>
-      <h2>Instamart</h2>
-      <p>It is instamart</p>
-      <h4>Assume that it has 100s of components inside it.</h4>
-    </>
+    <div>
+      <h1 className="text-3xl p-2 m-2">Instamart</h1>
+      <Section
+        title={"About Instamart"}
+        description={
+          "This is the about section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorumby Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
+        }
+      />
+      <Section
+        title={"Team Instamart"}
+        description={
+          "This is the team section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
+        }
+      />
+      <Section
+        title={"Careers"}
+        description={
+          "This is the team section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
+        }
+      />
+    </div>
   );
 };
 export default Instamart;
