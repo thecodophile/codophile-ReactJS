42dc89aa080be1569dc722c3098d0588bbe2e0e2
Somnath Dey <sdey.aklakhi@gmail.com>
2023-02-09 09:57:52 +0530

    start session 11

---
+++ 11. Data is the new Oil/src/components/Body.js
@@ -0,0 +1,93 @@
+import { restrautList } from "../constants";
+import RestruntCard from "./RestrauntCard";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+import { filterData } from "../utils/helper";
+import useOnline from "../utils/useOnline";
+
+const Body = () => {
+  const [allrestaurant, setAllRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [filteredrestaurants, setfilteredRestaurant] = useState([]);
+
+  useEffect(() => {
+    getRestaurnts();
+  }, []);
+
+  async function getRestaurnts() {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.572646&lng=88.36389500000001&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setfilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  }
+
+  // const isOnline = useOnline();
+
+  // if (!isOnline) {
+  //   return <h1>🛑Offline, please check your internet connection!</h1>;
+  // }
+
+  console.log("Render");
+
+  if (!allrestaurant) return null;
+
+  // const searchbtn = {
+  //   backgroundColor: "till",
+  //   color: "white",
+  // };
+
+  return allrestaurant.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <>
+      <div className="p-2 bg-pink-50 my-2 ">
+        <input
+          type="text"
+          className="focus:bg-green-50 p-2 m-2"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          // className="search-btn"
+          // style={searchbtn}
+          // style={{
+          //   backgroundColor: "teal",
+          //   color: "black",
+          // }}
+          className="text-xl text-white bg-purple-500 hover:bg-purple-700 hover:first-letter:font-bold  p-1 m-1 rounded "
+          onClick={(e) => {
+            const data = filterData(searchText, allrestaurant);
+            setfilteredRestaurant(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="flex flex-wrap">
+        {filteredrestaurants.length === 0 ? (
+          <h2>No Restaurnat match your Filter</h2>
+        ) : (
+          filteredrestaurants.map((restraut) => {
+            return (
+              <Link
+                to={"/restaurant/" + restraut.data.id}
+                key={restraut.data.id}
+              >
+                <RestruntCard {...restraut.data} />
+              </Link>
+            );
+          })
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Body;
---
+++ 11. Data is the new Oil/src/components/Header.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "../assets/foodvilla.jpg";
+import { Link } from "react-router-dom";
+import useOnline from "../utils/useOnline";
+
+export const Title = () => {
+  return (
+    <a href="/">
+      <img className="h-24 p-1" src={Logo} alt="logo image" />
+    </a>
+  );
+};
+
+const HeaderComponent = () => {
+  const [isLogedIn, setIsLogedIn] = useState(false);
+  const isOnline = useOnline();
+
+  return (
+    <div className="flex justify-between items-center bg-rose-200 shadow-lg">
+      <Title />
+
+      <div>
+        <ul className="flex py-10">
+          <li className="px-2">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-2">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="px-2">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="px-2">
+            <Link to="/instamart">Instamart</Link>
+          </li>
+          <li className="px-2">Cart</li>
+        </ul>
+      </div>
+      <h2>{isOnline ? "🟢" : "🔴"}</h2>
+      {isLogedIn ? (
+        <button onClick={() => setIsLogedIn(false)}>Logout</button>
+      ) : (
+        <button onClick={() => setIsLogedIn(true)}>Login</button>
+      )}
+    </div>
+  );
+};
+
+export default HeaderComponent;
