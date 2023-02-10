821a7e0c5b3c5f6949a76cb32229cc8872c0d799
Somnath Dey <sdey.aklakhi@gmail.com>
2023-02-10 13:26:00 +0530

    better way to create collapsible accordion

--- 11. Data is the new Oil/src/components/Instamart.js
+++ 11. Data is the new Oil/src/components/Instamart.js
@@ -6,7 +6,9 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
       <h2 className="text-xl font-bold">{title}</h2>
       {isVisible ? (
         <button
-          onClick={() => setIsVisible(false)}
+          onClick={() => {
+            setIsVisible(false);
+          }}
           className="border border-green-400 bg-green-200 cursor-pointer text-sm m-2"
         >
           Hide
@@ -26,13 +28,7 @@ const Section = ({ title, description, isVisible, setIsVisible }) => {
 };
 
 const Instamart = () => {
-  const [sectionConfig, setSectionConfig] = useState({
-    showAbout: false,
-    showTeam: false,
-    showCareers: false,
-    showProduct: false,
-    showDetails: false,
-  });
+  const [visibleSection, setVisibleSection] = useState("about");
   return (
     <div>
       <h1 className="text-3xl p-2 m-2">Instamart</h1>
@@ -41,80 +37,24 @@ const Instamart = () => {
         description={
           "This is the about section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorumby Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
         }
-        isVisible={sectionConfig.showAbout}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: true,
-            showTeam: false,
-            showCareers: false,
-            showProduct: false,
-            showDetails: false,
-          })
-        }
+        isVisible={visibleSection === "about"}
+        setIsVisible={() => setVisibleSection("about")}
       />
       <Section
         title={"Team Instamart"}
         description={
           "This is the team section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
         }
-        isVisible={sectionConfig.showTeam}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeam: true,
-            showCareers: false,
-            showProduct: false,
-            showDetails: false,
-          })
-        }
+        isVisible={visibleSection === "team"}
+        setIsVisible={() => setVisibleSection("team")}
       />
       <Section
         title={"Careers"}
         description={
           "This is the team section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
         }
-        isVisible={sectionConfig.showCareers}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeam: false,
-            showCareers: true,
-            showProduct: false,
-            showDetails: false,
-          })
-        }
-      />
-      <Section
-        title={"Product"}
-        description={
-          "This is the product section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
-        }
-        isVisible={sectionConfig.showProduct}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeam: false,
-            showCareers: false,
-            showProduct: true,
-            showDetails: false,
-          })
-        }
-      />
-      <Section
-        title={"Details"}
-        description={
-          "This is the Details section of instamart.The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
-        }
-        isVisible={sectionConfig.showDetails}
-        setIsVisible={() =>
-          setSectionConfig({
-            showAbout: false,
-            showTeam: false,
-            showCareers: false,
-            showProduct: false,
-            showDetails: true,
-          })
-        }
+        isVisible={visibleSection === "career"}
+        setIsVisible={() => setVisibleSection("career")}
       />
     </div>
   );
